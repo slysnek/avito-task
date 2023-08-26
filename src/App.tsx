f9d4a011cdeefd5eeec633b2fdd8b505842be1d8
Kirill <bloodwingest@gmail.com>
2023-08-26 18:17:46 +0300

    feat: add some routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Layout from './components/Layout';
+import GamePage from './pages/GamePage';
 
 export function App() {
   return (
     <>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="games/:id" element={<GamePage />} />
+        </Route>
       </Routes>
     </>
   );
