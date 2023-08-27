921032f5e68a9446fffde792a0041aa5b4fd2fb9
Kirill <bloodwingest@gmail.com>
2023-08-27 23:13:22 +0300

    feat: add go back button and condition if there are os requirements

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { api } from '../api/f2p-games-api';
 
 const GamePage = () => {
   const { id } = useParams();
-
+  const navigate = useNavigate();
   const [game, setGame] = useState(null);
+
+  const goBack = () => navigate(-1);
 
   useEffect(() => {
     const getGame = async () => {
@@ -19,19 +21,22 @@ const GamePage = () => {
     <div>
       {game && (
         <>
+          <button onClick={goBack}>Back</button>
           <h1>{game.title}</h1>
           <p>{game.release_date}</p>
           <p>{game.publisher}</p>
           <p>{game.developer}</p>
           <p>{game.genre}</p>
           <img src={game.thumbnail} alt="no picture :(" />
-          <ul>
-            <li>{game.minimum_system_requirements.os}</li>
-            <li>{game.minimum_system_requirements.processor}</li>
-            <li>{game.minimum_system_requirements.memory}</li>
-            <li>{game.minimum_system_requirements.graphics}</li>
-            <li>{game.minimum_system_requirements.storage}</li>
-          </ul>
+          {game.minimum_system_requirements ? (
+            <ul>
+              <li>{game.minimum_system_requirements.os}</li>
+              <li>{game.minimum_system_requirements.processor}</li>
+              <li>{game.minimum_system_requirements.memory}</li>
+              <li>{game.minimum_system_requirements.graphics}</li>
+              <li>{game.minimum_system_requirements.storage}</li>
+            </ul>
+          ) : <p>no data for requirements is provided</p>}
         </>
       )}
     </div>
