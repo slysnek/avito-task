3583a31f27d850318b4b5f383564cc241ac91893
Kirill <bloodwingest@gmail.com>
2023-08-29 02:07:47 +0300

    feat: complete view for game page

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { api } from '../api/f2p-games-api';
+import { Button, Typography, ConfigProvider, Collapse } from 'antd';
+import { LeftCircleOutlined } from '@ant-design/icons';
+import Carousel from '../components/Carousel';
+
+const { Title, Text } = Typography;
 
 const GamePage = () => {
   const { id } = useParams();
@@ -18,33 +23,69 @@ const GamePage = () => {
   }, [id]);
 
   return (
-    <div>
-      {game && (
-        <>
-          <button onClick={goBack}>Back</button>
-          <h1>{game.title}</h1>
-          <p>{game.release_date.split('-').reverse().join('/')}</p>
-          <p>{game.publisher}</p>
-          <p>{game.developer}</p>
-          <p>{game.genre}</p>
-          <img src={game.thumbnail} alt="no picture :(" />
-          {game.minimum_system_requirements ? (
-            <ul>
-              <li>{game.minimum_system_requirements.os}</li>
-              <li>{game.minimum_system_requirements.processor}</li>
-              <li>{game.minimum_system_requirements.memory}</li>
-              <li>{game.minimum_system_requirements.graphics}</li>
-              <li>{game.minimum_system_requirements.storage}</li>
-            </ul>
-          ) : (
-            <p>no data for requirements is provided</p>
-          )}
-        </>
-      )}
+    <div className="game-info-wrapper">
+      <ConfigProvider
+        theme={{
+          token: {
+            colorTextBase: '#c6d4df',
+            colorTextHeading: 'white',
+            colorPrimary: '#171d25',
+          },
+        }}
+      >
+        {game && (
+          <>
+            <Button
+              className="back-button"
+              onClick={goBack}
+              type="primary"
+              icon={<LeftCircleOutlined />}
+            >
+              Back
+            </Button>
+            <Title>{game.title}</Title>
+            <div className="upper-info-wrapper">
+              <Carousel images={game.screenshots.map((el) => el.image)}></Carousel>
+              <div className="main-info-wrapper">
+                <img className="game-thumbnail" src={game.thumbnail} alt="no picture :(" />
+                <div className="info-wrapper">
+                  <Title level={5}>Genre: {game.genre}</Title>
+                  <Title level={5}>
+                    Date released: {game.release_date.split('-').reverse().join('/')}
+                  </Title>
+                  <Title level={5}>Publisher: {game.publisher}</Title>
+                  <Title level={5}>Developer: {game.developer}</Title>
+                </div>
+              </div>
+            </div>
+            <Text>{game.description}</Text>
+            {game.minimum_system_requirements?.os ? (
+              <Collapse
+                ghost={true}
+                items={[
+                  {
+                    key: '1',
+                    label: 'Minimal System Requirements',
+                    children: (
+                      <ul>
+                        <li>{game.minimum_system_requirements.os}</li>
+                        <li>{game.minimum_system_requirements.processor}</li>
+                        <li>{game.minimum_system_requirements.memory}</li>
+                        <li>{game.minimum_system_requirements.graphics}</li>
+                        <li>{game.minimum_system_requirements.storage}</li>
+                      </ul>
+                    ),
+                  },
+                ]}
+              ></Collapse>
+            ) : (
+              <Title level={3}>No data for system requirements was provided by API.</Title>
+            )}
+          </>
+        )}
+      </ConfigProvider>
     </div>
   );
 };
-
-
 
 export default GamePage;
