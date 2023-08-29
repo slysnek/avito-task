b7ff434a66d6fd599cab9bf58b898f8c16dd5759
Kirill <bloodwingest@gmail.com>
2023-08-29 21:15:37 +0300

    refactor: replace token provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,28 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Layout from './components/Layout';
 import GamePage from './pages/GamePage';
+import { ConfigProvider } from 'antd';
 
 export function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="games/:id" element={<GamePage />} />
-        </Route>
-      </Routes>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorTextBase: '#c6d4df',
+            colorTextHeading: 'white',
+            colorPrimary: '#171d25',
+          },
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="games/:id" element={<GamePage />} />
+          </Route>
+        </Routes>
+      </ConfigProvider>
     </>
   );
 }
